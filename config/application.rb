30c756a299d75caf5118ccb960474c53aa137311
lbuell <liamcbuell@gmail.com>
2014-02-21 14:00:53 -0800

    ch3 exercises

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require "rulers"
 $LOAD_PATH << File.join(File.dirname(__FILE__),
                         "..", "app",
                         "controllers")
-# --> No more require here! <--
 module BestQuotes
   class Application < Rulers::Application
   end
